42482644ba863a3d41365b028149663b0c8d81a7
akbernamazi <akbarnamazi123@gmail.com>
2020-03-28 18:07:53 +0530

    adding server

---
+++ index.js
@@ -0,0 +1,25 @@
+const http=require('http');
+const fs=require('fs');
+const port=8000;
+
+function requestHandler(req,res){
+    console.log(req.url);
+    res.writeHead(200,{'content-type': 'text/html'});
+
+    fs.readFile('./index.html',function(err,data){
+        if(err){
+            console.log('error',err);
+            return res.end('<h1>Error</h1>');
+        }
+        return res.end(data);
+    });
+}
+const server=http.createServer(requestHandler);
+
+server.listen(port,function(err){
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log("Server is running",port);
+})
